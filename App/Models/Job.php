adbbd6287f40801bad81b6254f990d3493e5e20d
ingfredd <ingfrecab@gmail.com>
2019-02-28 00:01:38 -0500

    Add integration with databases

--- App/Models/Job.php
+++ App/Models/Job.php
@@ -1,15 +1,19 @@
 <?php
 namespace App\Models;
-require_once 'BaseElement.php';
+//require_once 'BaseElement.php';
+use Illuminate\Database\Eloquent\Model;
 
-class Job extends BaseElement {
-
-    public function __construct($title,$description){
-        $newTitle = 'Job: '.$title;
-        $this->title = $newTitle;
-    }
+class Job extends Model {
+    protected $table = 'jobs';
+    // public function __construct($title,$description){
+    //     $newTitle = 'Job: '.$title;
+    //     $this->title = $newTitle;
+    // }
 
     public function getDurationAsString($months) {
+
+        
+
         $years = floor($this->months / 12);
         $extraMonths = $this->months % 12;
       
--- addJob.php
+++ addJob.php
@@ -1,15 +1,18 @@
 <?php  
 
+require_once 'vendor/autoload.php';
+
     use Illuminate\Database\Capsule\Manager as Capsule;
+    use App\Models\Job;
 
 $capsule = new Capsule;
 
 $capsule->addConnection([
     'driver'    => 'mysql',
     'host'      => 'localhost',
-    'database'  => 'database',
+    'database'  => 'cursophp',
     'username'  => 'root',
-    'password'  => 'password',
+    'password'  => '',
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
@@ -21,9 +24,16 @@ $capsule->setAsGlobal();
 // Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
 $capsule->bootEloquent();
 
-var_dump($_GET);
-var_dump($_POST);
+//Obtenemos objetos  enviados por url
+// var_dump($_GET);
+// var_dump($_POST);
 
+if (!empty($_POST)) {
+    $job = new Job();
+$job->title = $_POST['title'];
+$job->description = $_POST['description'];
+$job->save();
+}
 
 ?>
 <!DOCTYPE html>
